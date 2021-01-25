7961e08717b0b0b0c06725f33c6898c3cf0d9791
Luiz Carlos B Pereira <carlinhospereira12@hotmail.com>
2021-01-25 00:40:41 -0300

    COMMIT INICIAL

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/modules/home/homePage';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ src/modules/home/homePage.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+    View,
+    Text,
+  } from 'react-native';
+
+
+const App: () => React$Node = () => {
+    return (
+      <>
+          <View>
+              <Text >Hello World</Text>
+          </View>
+      </>
+    );
+  };
+
+
+export default App;
